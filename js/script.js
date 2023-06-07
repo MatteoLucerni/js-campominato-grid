e82914c01ea9cc607e22948964c6eb45588bb70d
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-07 15:07:33 +0200

    completed (no bonuses)

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@ console.log('JS OK');
 
 // prendo gli elementi dal DOM
 
+const buttonPlay = document.getElementById('button-play');
+const grid = document.getElementById('grid');
+const difficulty = document.getElementById('difficulty');
+
+// parametri per la griglia
+
+const rows = 10;
+const cols = 10;
+const cellsNumber = rows * cols;
+console.log('Max cells: ' + cellsNumber);
+
+// Creo la cella
+
+const createCell = () => {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    return cell;
+};
+
+// genero un numero casuale
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min)) + min;
+
+// creo il ciclo
+
+for(let i = 0; i < cellsNumber; i++){
+    const cell = grid.appendChild(createCell());
+    cell.innerText = i + 1;
+    cell.addEventListener('click', function(){
+        cell.classList.toggle('clicked');
+    });
+}
